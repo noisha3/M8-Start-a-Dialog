3940c5b1f335b44e9837388336c9117359bb2737
noisha3 <nogobaleza@s.sfusd.edu>
2025-11-21 15:09:57 -0800

    M8L4 Completed

--- M08.start_a_dialog_workbook/lessons/dialogue.gd
+++ M08.start_a_dialog_workbook/lessons/dialogue.gd
@@ -11,50 +11,43 @@ var bodies := {
 	"pink": preload ("res://assets/pink.png")
 }
 
-## An array of dictionaries. Each dictionary has three properties:
-## - expression: a [code]Texture[/code] containing an expression
-## - text: a [code]String[/code] containing the text the character says
-## - character: a [code]Texture[/code] representing the character
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions["regular"],
-		"text": "I've been learning about [wave]Arrays and Dictionaries[/wave]",
-		"character": bodies["sophia"]
+		"text": "Who's the goat[wave]",
+		"character": bodies["sophia"],
+		"choices": {
+			"jayden h": 1,
+			"noah g": 3,
+		}
 	},
 	{
 		"expression": expressions["regular"],
-		"text": "How has it been going?",
-		"character": bodies["pink"]
+		"text": "No. Jayden H is not the goat",
+		"character": bodies["pink"],
+		"choices": {
+			"It's obviously Noah G": 3,
+			"No its Jayden H": 2,
+		}
 	},
 	{
 		"expression": expressions["sad"],
-		"text": "... Well... it is a little bit [shake]complicated[/shake]!",
-		"character": bodies["sophia"]
+		"text": "I said [shake]it's Noah G[/shake]!",
+		"character": bodies["sophia"],
+		"choices":{
+			"I've been saying it was Noah G": 0,
+			"It's Jayden H wdym": 1,
+		}
 	},
 	{
 		"expression": expressions["sad"],
-		"text": "Oh!",
-		"character": bodies["pink"]
+		"text": "It's Noah G cause the G stands for Goat ofcourse",
+		"character": bodies["pink"],
+		"choices": {"Told you so (Quit)": -1}
 	},
-	{
-		"expression": expressions["regular"],
-		"text": "I believe in you!",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "If you stick to it, you'll eventually make it!",
-		"character": bodies["pink"]
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "That's it! Let's [tornado freq=3.0][rainbow val=1.0]GOOOOOO!!![/rainbow][/tornado]",
-		"character": bodies["sophia"]
-	}
 ]
 
 
-## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var action_buttons_v_box_container: VBoxContainer = %ActionButtonsVBoxContainer
 ## Audio player that plays voice sounds while text is being written
@@ -66,56 +59,38 @@ var dialogue_items: Array[Dictionary] = [
 
 
 func _ready() -> void:
-	create_buttons({
-			"yes": 1,
-			"no": 2,
-	})
 	show_text(0)
 
+func create_buttons(choices_data: Dictionary) -> void:
+	for button in action_buttons_v_box_container.get_children():
+		button.queue_free()
+	for choice_text in choices_data:
+		var button := Button.new()
+		action_buttons_v_box_container.add_child(button)
+		button.text = choice_text
+		var target_line_idx: int = choices_data[choice_text]
+		if target_line_idx == - 1:
+			button.pressed.connect(get_tree().quit)
+		else:
+			button.pressed.connect(show_text.bind(target_line_idx))
 
-## Draws the current text to the rich text element
 func show_text(current_item_index: int) -> void:
-	# We retrieve the current item from the array
 	var current_item := dialogue_items[current_item_index]
-	# from the item, we extract the properties.
-	# We set the text to the rich text control
-	# And we set the appropriate expression texture
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
 	body.texture = current_item["character"]
-
-	# We set the initial visible ratio to the text to 0, so we can change it in the tween
+	create_buttons(current_item["choices"])
 	rich_text_label.visible_ratio = 0.0
-	# We create a tween that will draw the text
 	var tween := create_tween()
-	# A variable that holds the amount of time for the text to show, in seconds
-	# We could write this directly in the tween call, but this is clearer.
-	# We will also use this for deciding on the sound length
 	var text_appearing_duration: float = current_item["text"].length() / 30.0
-	# We show the text slowly
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
-	# We randomize the audio playback's start time to make it sound different
-	# every time.
-	# We obtain the last possible offset in the sound that we can start from
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
-	# We pick a random position on that length
 	var sound_start_position := randf() * sound_max_offset
-	# We start playing the sound
 	audio_stream_player.play(sound_start_position)
-	# We make sure the sound stops when the text finishes displaying
 	tween.finished.connect(audio_stream_player.stop)
-
-	# We animate the character sliding in.
+	
 	slide_in()
-
-	# Finally, we disable the next button until the text finishes displaying.
-
-
-
-## Progresses to the next slide.
-
-
-## Animates the character when they start talking
+	
 func slide_in() -> void:
 	var slide_tween := create_tween()
 	slide_tween.set_ease(Tween.EASE_OUT)
